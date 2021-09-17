2c600556e9a895473bb6a7ba6eeeeb5fd54f5969
DavidAKinney <60051165+DavidAKinney@users.noreply.github.com>
2021-09-17 12:06:20 -0500

    removing sound plays on clicks

--- force-app/main/default/lwc/simon/simon.js
+++ force-app/main/default/lwc/simon/simon.js
@@ -96,7 +96,6 @@ export default class Simon extends LightningElement {
     addGreen = function() {
         if (this.readyForInput) {
             this.userArray.push(1);
-            this.playGreenSound.play();
             this.checkArrays();
         }
     }
@@ -105,7 +104,6 @@ export default class Simon extends LightningElement {
     addRed = function() {
         if (this.readyForInput) {
             this.userArray.push(2);
-            this.playRedSound.play();
             this.checkArrays();
         }
     }
@@ -114,7 +112,6 @@ export default class Simon extends LightningElement {
     addBlue = function() {
         if (this.readyForInput) {
             this.userArray.push(3);
-            this.playBlueSound.play();
             this.checkArrays();
         }
     }
@@ -123,7 +120,6 @@ export default class Simon extends LightningElement {
     addYellow = function() {
         if (this.readyForInput) {
             this.userArray.push(4);
-            this.playYellowSound.play();
             this.checkArrays();
         }
     }
